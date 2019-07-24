f98f391029e798da472fd108648aaef2edf131fe
Casey Beaulac <beaulac.casey@gmail.com>
2019-07-24 12:36:44 -0400

    main javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
     console.log("doc is ready!");
 
-    //The core Firebase JS SDK is always required and must be listed first 
-
-
-
-  var firebaseConfig = {
+    var firebaseConfig = {
         apiKey: "AIzaSyDyy_gyIerPlYxa_Z8xTxYPk4YaNpsD7uQ",
         authDomain: "train-time-4a804.firebaseapp.com",
         databaseURL: "https://train-time-4a804.firebaseio.com",
@@ -18,7 +14,9 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
 
     var db = firebase.database();
-
+    var currentTime = moment();
+    console.log("current time :" + currentTime);
+    console.log(moment(currentTime, "X"));
     $("#addTrain").on("click", function (event) {
 
         var getName = $("#name-input").val().trim();
@@ -26,13 +24,48 @@ $(document).ready(function () {
         var getTime = $("#time-input").val().trim();
         var getFreq = $("#frequency-input").val().trim();
 
-        db.ref().set({
+        db.ref().push({
             name: getName,
             destination: getDestination,
             time: getTime,
             frequency: getFreq
 
         });
+
+    });
+
+    db.ref().on("child_added", function (snapshot) {
+        console.log(snapshot.val());
+
+        var snapName = snapshot.val().name;
+        var snapDestination = snapshot.val().destination;
+        var snapTime = snapshot.val().time;
+        var snapFreq = snapshot.val().frequency;
+
+        console.log(snapName);
+        console.log(snapDestination);
+        console.log(snapTime);
+        console.log(snapFreq);
+
+        var converted = moment(snapTime, "HH:mm").subtract(1, "years");
+        var difference = moment().diff(moment(converted), "minutes");
+        var remainder = difference % snapFreq;
+        var away = snapFreq - remainder;
+        var next = moment().add(away, "minutes");
+        next = moment(next).format("HH:mm");
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(snapName),
+            $("<td>").text(snapDestination),
+            $("<td>").text(snapFreq),
+            $("<td>").text(next),
+            $("<td>").text(away)
+        );
+
+        // Append the new row to the table
+        $("tbody").append(newRow);
+
+
 
     });
 
